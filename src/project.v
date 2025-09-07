fb3fcc340ba075392af3ef2b1f2e9b73bc3d6563
MonikaG <monika2011g@gmail.com>
2025-09-07 20:41:56 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,9 +19,9 @@ module tt_um_adder8 (
   // Internal wires
   wire [7:0] A = ui_in;    // First operand
   wire [7:0] B = uio_in;   // Second operand
-  wire [7:0] SUM; 
+  wire [7:0] SUM;
   wire Cout;
-  wire c1,c2,c3,c4,c5,c6,c7;
+  wire c1, c2, c3, c4, c5, c6, c7;
 
   // Instantiate 8 full adders
   fulladd FA0 (A[0], B[0], 1'b0, SUM[0], c1);
@@ -34,12 +34,12 @@ module tt_um_adder8 (
   fulladd FA7 (A[7], B[7], c7,   SUM[7], Cout);
 
   // Outputs
-  assign uo_out  = SUM;     // Final 8-bit sum
-  assign uio_out = 0;       // Not used
-  assign uio_oe  = 0;       // Not used
+  assign uo_out  = SUM;      // Final 8-bit sum
+  assign uio_out = 8'b0;      // Not used
+  assign uio_oe  = 8'b0;      // Not used
 
   // Prevent unused signal warnings
-  wire _unused = &{ena, clk, rst_n, Cout, 1'b0};
+  wire _unused_ok = &{ena, clk, rst_n, Cout};
 
 endmodule
 
@@ -48,6 +48,6 @@ module fulladd(
     input  wire x, y, carryin,
     output wire s, carryout
 );
-  assign s       = x ^ y ^ carryin;
+  assign s        = x ^ y ^ carryin;
   assign carryout = (x & y) | (y & carryin) | (carryin & x);
 endmodule
